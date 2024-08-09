c87493b914f247790a2dba7ce3d87e114b78b43e
NguyenVanCanh <ngcanh1236@gmail.com>
2024-08-09 16:33:28 +0700

    init project

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+from flask_login import LoginManager
+import cloudinary
+
+app = Flask(__name__)
+
+connect_string = "mysql+pymysql://root:%s@localhost/toca_db?charset=utf8mb4" % quote("Myca@1236")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = connect_string
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config['SECRET_KEY'] = 'askrdghasdjfgakdsfuhgjhdsLGHU'
+
+db = SQLAlchemy(app=app)
+login = LoginManager(app=app)
+
+cloudinary.config(
+    cloud_name="dbd7vfk12",
+    api_key="381798527745373",
+    api_secret="mq7kD-ynrQsabeC3zUXc5zHuDIY"
+)
---
+++ app/index.py
@@ -0,0 +1,10 @@
+from app import app
+
+
+@app.route('/')
+def index():
+    return 'Hello world!!!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
